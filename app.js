d094612200ef6480e1ef9c9550428aa2416e91b2
Marta <m.matysek95@wp.pl>
2020-09-21 20:40:47 +0200

    module pattern implemented
    module pattern implemented

---
+++ app.js
@@ -0,0 +1,11 @@
+var budgetControler = (function() {
+    // todo
+})();
+
+var uiControler = (function() {
+    //todo
+})();
+
+var globalControler = (function(budgetControl, uiControl){
+    //todo
+})(budgetControler, uiControler);
