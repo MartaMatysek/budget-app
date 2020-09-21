afd009eb11e7ff779dada4464fc197f4111d2ff2
Marta <m.matysek95@wp.pl>
2020-09-21 20:59:53 +0200

    readInputs functionality added

--- app.js
+++ app.js
@@ -3,9 +3,46 @@ var budgetControler = (function() {
 })();
 
 var uiControler = (function() {
-    //todo
+
+    var DOMValues = {
+        inputType: '.add-type',
+        inputDescription: '.add-description',
+        inputValue: '.add-value',
+        addButton: '.btn-add'
+    }
+
+    return {
+
+        getDOMValues: function() {
+            return DOMValues;
+        },
+
+        readInputs: function() {
+            return { 
+                type: document.querySelector(DOMValues.inputType).value,
+                description: document.querySelector(DOMValues.inputDescription).value,
+                value: document.querySelector(DOMValues.inputValue).value
+            };
+        }
+    };
+
 })();
 
 var globalControler = (function(budgetControl, uiControl){
-    //todo
+
+    var DOM = uiControl.getDOMValues();
+
+    var addItem = function() {
+        var inputs = uiControl.readInputs();
+        console.log(inputs);
+    }
+
+    document.querySelector(DOM.addButton).addEventListener('click', addItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.key === "Enter") {
+            addItem();
+        } 
+    })
+
 })(budgetControler, uiControler);
